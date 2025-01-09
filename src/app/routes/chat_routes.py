3fcfbffd6f563184999ce9adac0846702a3c2f58
HuzaifaRizwan1231 <huzaifa.rizwan1231@gmail.com>
2025-01-09 18:53:32 +0500

    Add support for MergeStack Chat Assistant and enhance OpenAI integration

--- src/app/routes/chat_routes.py
+++ src/app/routes/chat_routes.py
@@ -5,7 +5,7 @@ import openai
 from config import GEMINI_API_KEY, OPENAI_API_KEY, ALLOWED_OPENAI_MODELS, ALLOWED_GEMINI_MODELS
 from schemas.chat_schema import chatCompletionRequestSchema
 from services.gemini_service import geminiChatCompletion
-from services.openai_service import openaiChatCompletion
+from services.openai_service import openaiChatCompletion, openaiMergestackChatAssistant
 from utils.response_builder import ResponseBuilder
 
 
@@ -22,7 +22,9 @@ def chatCompletion(body: chatCompletionRequestSchema):
     if body.model in ALLOWED_GEMINI_MODELS:
         return geminiChatCompletion(body.model, body.text)
     elif body.model in ALLOWED_OPENAI_MODELS:
-        return openaiChatCompletion(body.model, body.text)
+        return openaiChatCompletion(body.model, body.text) 
+    elif body.model == "mergestack-chat-assistant":
+        return openaiMergestackChatAssistant("gpt-4o",body.text)
     else:
         return ResponseBuilder().setSuccess(False).setMessage("Invalid model").setStatusCode(400).build()
 
---
+++ src/app/utils/mergestack_chat_assistant.py
@@ -0,0 +1,39 @@
+import openai,os
+
+class MergeStackChatAssistant:
+    def __init__(self, model):  
+        # Thread creation
+        self.thread = openai.beta.threads.create()
+        
+        # Vector store creation
+        vector_store = openai.beta.vector_stores.create(name="MergeStack Policy Store")
+        base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+        file_paths = [
+            os.path.join(base_dir, "files", "mergestack_policy.pdf")
+        ]
+        file_streams = [open(path, "rb") for path in file_paths]
+        file_batch = openai.beta.vector_stores.file_batches.upload_and_poll(
+        vector_store_id=vector_store.id, files=file_streams
+        )
+        
+        # Assistant creation
+        self.my_assistant = openai.beta.assistants.create(
+            instructions="Your name is MergeGPT and you are an expert HR assistant for Mergestack. Use your knowledge base to answer any queries regarding the company, its policies and procedures. All this responses should follow proper markdown formatting.",
+            name="MergGPT",
+            tools=[{"type":"file_search"}],
+            model=model,
+            tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
+        )
+
+    def pollAndRun(self):
+        run = openai.beta.threads.runs.create(
+            thread_id=self.thread.id,
+            assistant_id=self.my_assistant.id,
+        )
+        
+        while run.status != "completed":
+            run = openai.beta.threads.runs.retrieve(thread_id=self.thread.id,run_id= run.id)
+            
+        return openai.beta.threads.messages.list(thread_id=self.thread.id).data[0]
+
+   
